3172331a4cce17040428ac38be27156d86faee0b
linfeng <linfeng@eqxiu.com>
2021-01-27 20:27:08 +0800

    优化TDMQ注解，优化订阅

--- tdmq-spring-boot-starter-demo/src/main/java/com/eqxiu/tdmq/example/DemoHandler.java
+++ tdmq-spring-boot-starter-demo/src/main/java/com/eqxiu/tdmq/example/DemoHandler.java
@@ -1,24 +1,24 @@
-//package com.eqxiu.tdmq.example;
-//
-//import com.eqxiu.soc.iap.mns.dto.ElementCheckTodoDto;
-//import com.seelyn.tdmq.TdmqBatchListener;
-//import com.seelyn.tdmq.annotation.TdmqHandler;
-//import com.seelyn.tdmq.annotation.TdmqTopic;
-//import com.seelyn.tdmq.exception.MessageRedeliverException;
-//import org.apache.pulsar.client.api.Consumer;
-//import org.apache.pulsar.client.api.Messages;
-//import org.springframework.stereotype.Component;
-//
-///**
-// * @author linfeng
-// */
-//@Component
-//@TdmqHandler(topics = {@TdmqTopic(topic = "${eqxiu.scs.mns.topics.content-todo-general.topic}",
-//        tags = "${eqxiu.scs.mns.topics.content-todo-general.tags}")})
-//public class DemoHandler implements TdmqBatchListener<ElementCheckTodoDto> {
-//
-//    @Override
-//    public void received(Consumer<ElementCheckTodoDto> consumer, Messages<ElementCheckTodoDto> messages) throws MessageRedeliverException {
-//        System.out.println(messages.size());
-//    }
-//}
+package com.eqxiu.tdmq.example;
+
+import com.eqxiu.soc.iap.mns.dto.ElementCheckTodoDto;
+import com.seelyn.tdmq.TdmqBatchListener;
+import com.seelyn.tdmq.annotation.TdmqHandler;
+import com.seelyn.tdmq.annotation.TdmqTopic;
+import com.seelyn.tdmq.exception.MessageRedeliverException;
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Messages;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author linfeng
+ */
+@Component
+@TdmqHandler(topics = {@TdmqTopic(topic = "${eqxiu.scs.mns.topics.content-todo-general.topic}",
+        tags = "${eqxiu.scs.mns.topics.content-todo-general.tags}")})
+public class DemoHandler implements TdmqBatchListener<ElementCheckTodoDto> {
+
+    @Override
+    public void received(Consumer<ElementCheckTodoDto> consumer, Messages<ElementCheckTodoDto> messages) throws MessageRedeliverException {
+        System.out.println(messages.size());
+    }
+}
